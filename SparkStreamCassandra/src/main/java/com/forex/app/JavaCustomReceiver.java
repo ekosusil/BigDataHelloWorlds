10a9a5e26f444c052b333f50403f42a082749fb1
ekosusil <eko.harmawan.susilo@gmail.com>
2016-03-18 04:25:07 -0700

    Initial Commit

---
+++ SparkStreamCassandra/src/main/java/com/forex/app/JavaCustomReceiver.java
@@ -0,0 +1,169 @@
+package com.forex.app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.util.EntityUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.storage.StorageLevel;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.receiver.Receiver;
+import org.apache.spark.api.java.function.Function2;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.apache.spark.streaming.Time;
+
+import com.forex.model.ForexData;
+import com.forex.util.FunctionHelpers;
+
+public class JavaCustomReceiver extends Receiver<String> {
+
+	private static final String TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS";
+
+	public static void main(String args[]) {
+
+		SparkConf conf = new SparkConf().setAppName("Test Custom Receiver")
+				.setMaster("local[2]");
+		JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(
+				1000));
+
+		JavaReceiverInputDStream<String> lines = ssc
+				.receiverStream(new JavaCustomReceiver(" ",
+						" "));
+
+		JavaDStream<ForexData> fxData = lines
+				.map(new Function<String, ForexData>() {
+
+					public ForexData call(String responseLine) throws Exception {
+						ForexData _result = new ForexData();
+
+						if (responseLine != null) {
+							Object obj = JSONValue.parse(responseLine);
+							if (obj != null) {
+								JSONObject tick = (JSONObject) obj;
+
+								if (tick.containsKey("tick")) {
+									tick = (JSONObject) tick.get("tick");
+									String instrument = tick.get("instrument")
+											.toString();
+									String strTime = tick.get("time")
+											.toString();
+
+									long time = FunctionHelpers
+											.parseStringTimeToLong(TIME_FORMAT,
+													strTime);
+									double bid = Double.parseDouble(tick.get(
+											"bid").toString());
+									double ask = Double.parseDouble(tick.get(
+											"ask").toString());
+									_result = new ForexData(instrument, bid,
+											ask, time);
+									// service.store(_result);
+
+									System.out.println(_result);
+								}
+							} else {
+								System.out
+										.println("Error during parsing the response "
+												+ responseLine);
+							}
+
+						}
+
+						return _result;
+					}
+
+				});
+		fxData.foreachRDD(new VoidFunction<JavaRDD<ForexData>>(){
+
+			public void call(JavaRDD<ForexData> arg0) throws Exception {
+				
+				List<ForexData> dt=arg0.collect();
+				for(ForexData fxDt:dt)
+					System.out.println(fxDt);
+			}
+			
+		});
+
+		ssc.start();
+		ssc.awaitTermination();
+	}
+
+	private String userId;
+	private String key;
+	private String instrument;
+
+	public JavaCustomReceiver(String userId, String key) {
+		super(StorageLevel.MEMORY_AND_DISK_2());
+
+		this.userId = userId;
+		this.key = key;
+		this.instrument = "EUR_USD";
+	}
+
+	@Override
+	public void onStart() {
+		new Thread() {
+			@Override
+			public void run() {
+				receive();
+			}
+		}.start();
+
+	}
+
+	private void receive() {
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpUriRequest httpGet = new HttpGet(
+				"https://stream-fxpractice.oanda.com/v1/prices?accountId="
+						+ userId + "&instruments=" + instrument);
+		httpGet.setHeader(new BasicHeader("Authorization", "Bearer " + key));
+
+		System.out.println("Executing request: " + httpGet.getRequestLine());
+
+		try {
+			HttpResponse resp = httpClient.execute(httpGet);
+			HttpEntity entity = resp.getEntity();
+
+			if (resp.getStatusLine().getStatusCode() == 200 && entity != null) {
+				InputStream stream = entity.getContent();
+				String line;
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						stream));
+
+				while ((line = br.readLine()) != null) {
+					this.store(line);
+				}
+			} else {
+
+				String responseString = EntityUtils.toString(entity, "UTF-8");
+				System.out.println("ERROR RESPONSE " + responseString);
+			}
+
+		} catch (IOException e) {
+			System.out.println("EXCEPTION when reading service");
+		}
+
+	}
+
+	@Override
+	public void onStop() {
+	}
+
+}
---
+++ SparkStreamCassandra/src/main/java/com/forex/model/ForexData.java
@@ -0,0 +1,122 @@
+package com.forex.model;
+
+
+
+import java.util.Date;
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class ForexData implements Serializable {
+	private static final String INSTRUMENT_DATE_SEPARATOR_ROW_KEY_HBASE=";";
+	private static Logger logger = LoggerFactory.getLogger(ForexData.class);
+	public ForexData() {
+	}
+
+	public ForexData(String instrument, double buyPrice, double sellPrice,
+			long timeStamp) {
+		this.instrument = instrument;
+		this.buyPrice = buyPrice;
+		this.sellPrice = sellPrice;
+		this.timeStamp = timeStamp;
+	}
+
+	
+
+	private String instrument;
+	private double buyPrice;
+	private double sellPrice;
+	private long timeStamp;
+	
+	public Date getDate() {
+		Date _result = null;
+		_result = new Date(timeStamp);
+		return _result;
+	}
+
+	public String getTimeAsString() {
+		SimpleDateFormat formatter = new SimpleDateFormat("hh:mm:ss");
+		return formatter.format(getDate());
+	}
+
+	public String getDateAsString() {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd:MM:yyyy");
+		return formatter.format(getDate());
+
+	}
+	
+	public void parseRowKey(String str){
+		if (str!=null && str.contains(INSTRUMENT_DATE_SEPARATOR_ROW_KEY_HBASE)){
+			String tokens[]=str.split(INSTRUMENT_DATE_SEPARATOR_ROW_KEY_HBASE);
+			this.setInstrument(tokens[0]);
+		}
+	}
+
+	public String getInstrument() {
+		return instrument;
+	}
+
+	public void setInstrument(String instrument) {
+		this.instrument = instrument;
+	}
+
+	public double getBuyPrice() {
+		return buyPrice;
+	}
+
+	public void setBuyPrice(double buyPrice) {
+		this.buyPrice = buyPrice;
+	}
+
+	public double getSellPrice() {
+		return sellPrice;
+	}
+
+	public void setSellPrice(double sellPrice) {
+		this.sellPrice = sellPrice;
+	}
+
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public String createRowKey(){
+		return this.instrument+INSTRUMENT_DATE_SEPARATOR_ROW_KEY_HBASE+this.getDateAsString();
+	}
+	
+	@Override
+	public String toString() {
+		return "ForexData [instrument=" + instrument + ", buyPrice=" + buyPrice
+				+ ", sellPrice=" + sellPrice + ", timeStamp=" + timeStamp + "]";
+	
+	}
+	@Override
+	public boolean equals(Object object){
+		
+		if (object==null)
+			return false;
+		else if(!(object instanceof ForexData))
+			return false;
+		else{
+			ForexData that=(ForexData)object;
+			return (that.instrument.equals(this.instrument) && that.timeStamp == this.timeStamp  && that.buyPrice==this.buyPrice && that.sellPrice == this.sellPrice);
+		}
+		
+	}
+
+	
+
+	
+	
+
+
+}
